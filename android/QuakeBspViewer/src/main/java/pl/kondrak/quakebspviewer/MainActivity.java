860fd4cbced995753f9127c549139b39752c470d
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2018-08-28 15:05:11 +0200

    Removed unused native function.

--- android/QuakeBspViewer/src/main/java/pl/kondrak/quakebspviewer/MainActivity.java
+++ android/QuakeBspViewer/src/main/java/pl/kondrak/quakebspviewer/MainActivity.java
@@ -14,10 +14,4 @@ public class MainActivity extends NativeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
