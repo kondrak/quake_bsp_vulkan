c86307fe2e4c81d9a9053b5cf332bc1c3f55ba0f
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2018-08-29 14:20:39 +0200

    Use SDL2 infrastructure to build Android app.

--- android/QuakeBspViewer/src/main/java/pl/kondrak/quakebspviewer/MainActivity.java
+++ android/QuakeBspViewer/src/main/java/pl/kondrak/quakebspviewer/MainActivity.java
@@ -1,17 +1,7 @@
 package pl.kondrak.quakebspviewer;
 
-import android.app.NativeActivity;
-import android.os.Bundle;
+import org.libsdl.app.SDLActivity;
 
-public class MainActivity extends NativeActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+public class MainActivity extends SDLActivity
+{
 }
