d284ce8bc6a49beefb169659a2d3170633f744ca
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2018-08-30 10:03:21 +0200

    Load Vulkan and allocate AssetManager.

--- android/QuakeBspViewer/src/main/java/pl/kondrak/quakebspviewer/MainActivity.java
+++ android/QuakeBspViewer/src/main/java/pl/kondrak/quakebspviewer/MainActivity.java
@@ -1,7 +1,26 @@
 package pl.kondrak.quakebspviewer;
 
+import android.content.res.AssetManager;
+import android.os.Bundle;
 import org.libsdl.app.SDLActivity;
 
 public class MainActivity extends SDLActivity
 {
+    private AssetManager assetMgr;
+
+    @Override
+    protected String[] getLibraries() {
+        return new String[] {
+            "SDL2",
+            "vulkan",
+            "main"
+        };
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        assetMgr = getResources().getAssets();
+    }
 }
