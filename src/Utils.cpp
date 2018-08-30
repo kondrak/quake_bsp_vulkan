87aeaed6fd9a7c3d2a441e920264646d16c4f08f
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2018-08-30 13:04:48 +0200

    Error logger for Android.

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,12 +2,17 @@
 #ifdef _WIN32
 #include <Windows.h>
 #endif
+#ifdef __ANDROID__
+#include <android/log.h>
+#endif
 
 void LogError(const char *msg)
 {
     // basic error logging for VS debugger
-#ifdef _WIN32
+#if defined _WIN32
     OutputDebugStringA(msg);
+#elif defined __ANDROID__
+    __android_log_print(ANDROID_LOG_ERROR, "QuakeBspViewer", "%s\n", msg);
 #else
     printf("%s\n", msg);
 #endif
