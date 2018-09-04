bbcf42146e33aa20092eda46454ff73992e7ba8b
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2018-09-04 19:08:26 +0200

    Use Vulkan 1.0 on iOS.

--- src/renderer/vulkan/Base.cpp
+++ src/renderer/vulkan/Base.cpp
@@ -32,8 +32,8 @@ namespace vk
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "custom";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-// Android is limited to Vulkan 1.0 so far
-#ifdef __ANDROID__
+// Android and iOS are limited to Vulkan 1.0 at this point
+#if defined(__ANDROID__) || defined(TARGET_OS_IPHONE)
         appInfo.apiVersion = VK_API_VERSION_1_0;
 #else
         appInfo.apiVersion = VK_API_VERSION_1_1;
